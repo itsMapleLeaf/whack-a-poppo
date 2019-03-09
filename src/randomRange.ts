5a0e94a798952486bca395fdae065f875dc8d001
kingdaro <kingdaro@gmail.com>
2019-03-08 23:59:51 -0600

    add randomRange variant for returning ints

--- src/randomRange.ts
+++ src/randomRange.ts
@@ -1,3 +1,7 @@
 export default function randomRange(min: number, max: number) {
   return Math.random() * (max - min) + min
 }
+
+randomRange.int = function randomRangeInt(min: number, max: number) {
+  return Math.floor(randomRange(min, max))
+}
