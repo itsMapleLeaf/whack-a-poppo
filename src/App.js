3e05969ef427b2c7d08c0c6a9c6fa456368b3149
kingdaro <kingdaro@gmail.com>
2019-03-08 22:05:51 -0600

    e r a s e

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
 
 class App extends Component {
   render() {
@@ -21,8 +19,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
